1a168e22bb8295fd53e50d02124bc0a1b08e7f45
yutopp <yutopp@gmail.com>
2021-05-02 18:45:56 +0900

    Add an example

---
+++ Assets/VGltfExamples/Dynamic/Scripts/Loader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using VGltf;
+using VGltf.Unity;
+
+namespace VGltfExamples.Dynamic
+{
+    public class Loader : MonoBehaviour
+    {
+        [SerializeField] public string FilePath = "SampleModels/Alicia/VRM/AliciaSolid.vrm";
+
+        void Start()
+        {
+            GltfContainer gltfContainer;
+            using (var fs = new FileStream(FilePath, FileMode.Open))
+            {
+                gltfContainer = GltfContainer.FromGlb(fs);
+            }
+            
+            var gltfImporter = new Importer(gltfContainer);
+
+            var go = new GameObject();
+            gltfImporter.ImportSceneNodes(go);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
